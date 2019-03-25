6c53eb19d29800d819c61b894c69f85ccd79b588
Raman Shyshniou <rommer@ibuffed.com>
2019-03-25 11:47:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Builds the latest [**motion**](https://github.com/Motion-Project/motion.git) code from the github repository.
+
+```
+docker run -ti --rm -v /path/to/motion.conf:/etc/motion/motion.conf:ro 004helix/motion
+```
---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd /opt
+apk --no-cache add tini libmicrohttpd libjpeg-turbo ffmpeg-libs
+apk --no-cache add git gcc libc-dev libmicrohttpd-dev libjpeg-turbo-dev ffmpeg-dev autoconf automake
+
+git clone --depth 1 https://github.com/Motion-Project/motion.git src
+cd src && autoreconf -fiv && ./configure --prefix /usr/local && make && make install
+cd /opt && rm -rf src
+
+apk --no-cache del git gcc libc-dev libmicrohttpd-dev libjpeg-turbo-dev ffmpeg-dev autoconf automake
