02b619c851f6d946e46baef3968aa80c8402cd09
Raman Shyshniou <rommer@ibuffed.com>
2019-03-25 13:49:00 +0000

    Added mosquitto-clients

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-cd /opt
-apk --no-cache add tini libmicrohttpd libjpeg-turbo ffmpeg-libs
-apk --no-cache add git gcc make libc-dev libmicrohttpd-dev libjpeg-turbo-dev ffmpeg-dev autoconf automake
+set -ex
 
-git clone --depth 1 https://github.com/Motion-Project/motion.git src
-cd src && autoreconf -fiv && ./configure --prefix /usr/local && make && make install
+cd /opt
+apk --no-cache add tini libmicrohttpd libjpeg-turbo ffmpeg-libs sqlite-libs mosquitto-clients
+apk --no-cache add git gcc make libc-dev libmicrohttpd-dev libjpeg-turbo-dev ffmpeg-dev sqlite-dev autoconf automake
+
+git clone --depth 1 https://github.com/Motion-Project/motion.git src && cd src
+autoreconf -fiv && ./configure --prefix /usr/local && make && make install
 cd /opt && rm -rf src
 
-apk --no-cache del git gcc make libc-dev libmicrohttpd-dev libjpeg-turbo-dev ffmpeg-dev autoconf automake
+apk --no-cache del git gcc make libc-dev libmicrohttpd-dev libjpeg-turbo-dev ffmpeg-dev sqlite-dev autoconf automake
