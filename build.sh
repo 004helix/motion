cf145ed85456c341e1de16ec7335795e6156d37d
Raman Shyshniou <rommer@ibuffed.com>
2019-03-25 11:51:58 +0000

    Add make

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 cd /opt
 apk --no-cache add tini libmicrohttpd libjpeg-turbo ffmpeg-libs
-apk --no-cache add git gcc libc-dev libmicrohttpd-dev libjpeg-turbo-dev ffmpeg-dev autoconf automake
+apk --no-cache add git gcc make libc-dev libmicrohttpd-dev libjpeg-turbo-dev ffmpeg-dev autoconf automake
 
 git clone --depth 1 https://github.com/Motion-Project/motion.git src
 cd src && autoreconf -fiv && ./configure --prefix /usr/local && make && make install
 cd /opt && rm -rf src
 
-apk --no-cache del git gcc libc-dev libmicrohttpd-dev libjpeg-turbo-dev ffmpeg-dev autoconf automake
+apk --no-cache del git gcc make libc-dev libmicrohttpd-dev libjpeg-turbo-dev ffmpeg-dev autoconf automake
