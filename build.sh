0839774978b84900f36cf35fed87aebdcaacb82c
Raman Shyshniou <rommer@ibuffed.com>
2019-03-26 22:05:51 +0000

    Add fflags nobuffer for rtsp streams

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ apk --no-cache add tini libmicrohttpd libjpeg-turbo ffmpeg-libs sqlite-libs mosq
 apk --no-cache add git gcc make libc-dev libmicrohttpd-dev libjpeg-turbo-dev ffmpeg-dev sqlite-dev autoconf automake
 
 git clone --depth 1 https://github.com/Motion-Project/motion.git src && cd src
+sed -i netcam_rtsp.c -e '/"rtsp_transport"/i \        av_dict_set(&rtsp_data->opts, "fflags", "nobuffer", 0);'
 autoreconf -fiv && ./configure --prefix /usr/local && make && make install
 cd /opt && rm -rf src
 
